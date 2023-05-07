5441c79672f34e13292d38c557a4dfff36a6a7f6
Melisa <melnguyen11@gmail.com>
2023-05-07 17:44:18 -0500

    Navbar

---
+++ control-website/src/components/Navbar.js
@@ -0,0 +1,68 @@
+
+import { Button } from './Button';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu =() => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+        setButton(false);
+        } else 
+            {setButton(true);
+            }
+    };
+
+    window.addEventListener('resize', showButton);
+
+
+
+    return (
+    
+        <>
+        <nav className="navbar">
+            <div className = "navbar-container">
+                <Link to = "/" className = "navbar-logo">
+                    <i className = "fa-solid fa-shield-cat" />
+                </Link>
+                <div className = "menu-icon" onClick={handleClick}>
+                    <i className = {click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className = 'nav-item'>
+                        <Link to = '/' className = 'nav-links' onClick={closeMobileMenu}>
+                            Home
+                            </Link>
+                    </li>
+                    <li className = 'nav-item'>
+                        <Link to = '/homicide' className = 'nav-links' onClick={closeMobileMenu}>
+                            Homicides
+                            </Link>
+                    </li>
+                    <li className = 'nav-item'>
+                        <Link to = '/school' className = 'nav-links' onClick={closeMobileMenu}>
+                            Schoot Shootings
+                            </Link>
+                    </li>
+                    <li className = 'nav-item'>
+                        <Link to = '/provision' className = 'nav-links' onClick={closeMobileMenu}>
+                            Gun Provisions
+                            </Link>
+                    </li>
+                </ul>
+                {button && <Button buttonStyle='btn--outline'>CONTACT SENATOR</Button>}
+     
+            </div>
+        </nav>
+        
+        </>
+  );
+}
+
+export default Navbar
---
+++ installation.md
@@ -0,0 +1,9 @@
+### Terminal Commands 
+// Create project -- be sure to be in the correct folder
+$    npx create-react-app <project-name> 
+// Install yarn
+$   npm install --global yarn
+$   yarn --version
+
+$   npm install react-router-dom
+$   npm install react-bootstrap
